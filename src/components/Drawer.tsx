812b3a131c00a65ee7c1d11f5c3f3044ab8cbdc1
Julien Catania <julien.catania@gmail.com>
2023-02-27 09:51:28 +0100

    adding a more sexy drawer ...

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,6 +1,6 @@
 import Clips from '@/components/Clips';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import UsbIcon from '@mui/icons-material/Usb';
 
@@ -19,66 +19,65 @@ interface IDrawer {
 const Drawer = ({onPathSelected}: IDrawer) => {
     const [volumes, setVolumes] = useState<string[]>();
     const [source, setSource] = useState<string>();
-    const [open, setOpen] = useState<boolean>();
+    const [open, setOpen] = useState<boolean>(true);
 
     useEffect(() => {
         updateSources();
     }, [])
 
     const updateSources = () => {
+        // @ts-ignore
         window.sentinel.getFiles('/Volumes')
             .then((vols: string[]) => vols.filter(vol => vol.indexOf('Macintosh HD') === -1 && vol.indexOf('com.apple.') === -1))
             .then(setVolumes)
     }
     return <>
-        <IconButton variant="plain"
-                    onClick={(e: any) => {
-                        e.stopPropagation();
-                        setOpen(true);
-                    }}>
-            <ArrowForwardIosIcon/>
-        </IconButton>
-        <Stack spacing={1} className={open ? 'opened' : ''} alignItems="flex-start">
+        <Stack direction="row"
+               justifyContent="space-between"
+               alignItems="flex-start"
+               className={open ? 'opened' : ''}>
+
+            <Stack spacing={1} alignItems="flex-start">
+                <Select placeholder="Source"
+                        endDecorator={
+                            <RefreshIcon onMouseDown={(event) => {
+                                // don't open the popup when clicking on this button
+                                event.stopPropagation();
+                            }}
+                                         onClick={() => {
+                                             updateSources();
+                                         }}/>
+                        }
+                        onChange={(e, val) => setSource(val as string)}>
+
+                    {volumes?.map(vol => {
+                        const name = vol.split('/')[2];
+                        return <Option key={vol} value={vol}>
+                            <ListItemDecorator>
+                                <UsbIcon color={name === 'TESLADRIVE' ? 'success' : undefined}/>
+                            </ListItemDecorator>
+                            {name}
+                        </Option>
+                    })}
+
+                    <Option value={ROOT_PATH}>{ROOT_PATH}</Option>
+
+                </Select>
+                {source && <Clips path={source + SENTRY_PATH}
+                                  onSelection={path => {
+                                      setOpen(false);
+                                      onPathSelected(path);
+                                  }
+                                  }/>}
+            </Stack>
+
             <IconButton variant="plain"
-                        color="neutral"
                         onClick={(e: any) => {
                             e.stopPropagation();
-                            setOpen(false);
+                            setOpen(prev => !prev);
                         }}>
-                <MenuOpenIcon/>
+                {open ? <ArrowBackIosNewIcon/> : <ArrowForwardIosIcon/>}
             </IconButton>
-
-            <Select placeholder="Source"
-                    endDecorator={
-                        <RefreshIcon onMouseDown={(event) => {
-                            // don't open the popup when clicking on this button
-                            event.stopPropagation();
-                        }}
-                                     onClick={() => {
-                                         updateSources();
-                                     }}/>
-                    }
-                    onChange={(e, val) => setSource(val as string)}>
-
-                {volumes?.map(vol => {
-                    const name = vol.split('/')[2];
-                    return <Option key={vol} value={vol}>
-                        <ListItemDecorator>
-                            <UsbIcon color={name === 'TESLADRIVE' ? 'success' : undefined}/>
-                        </ListItemDecorator>
-                        {name}
-                    </Option>
-                })}
-
-                <Option value={ROOT_PATH}>{ROOT_PATH}</Option>
-
-            </Select>
-            {source && <Clips path={source + SENTRY_PATH}
-                              onSelection={path => {
-                                  setOpen(false);
-                                  onPathSelected(path);
-                              }
-                              }/>}
         </Stack>
     </>
 }
